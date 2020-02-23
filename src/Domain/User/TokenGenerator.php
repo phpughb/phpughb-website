e1edecee07e4cba38bbe0802ce96cc1494ab5c67
Lucas Nothnagel <lucas.nothnagel@sensiolabs.de>
2020-02-23 23:06:29 +0100

    Add real token generation

--- src/Domain/User/TokenGenerator.php
+++ src/Domain/User/TokenGenerator.php
@@ -6,8 +6,18 @@ namespace App\Domain\User;
 
 class TokenGenerator
 {
-    public function generate(): string
+    private const ALLOWED_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    public function generate(int $length = 30, string $allowedChars = self::ALLOWED_CHARACTERS): string
     {
-        return 'test';
+        $random = '';
+        $chars = str_split($allowedChars);
+        $allowedCharsLength = count($chars);
+        for ($i = 0; $i < $length; ++$i) {
+            $index = random_int(0, $allowedCharsLength - 1);
+            $random .= $allowedChars[$index];
+        }
+
+        return $random;
     }
 }
