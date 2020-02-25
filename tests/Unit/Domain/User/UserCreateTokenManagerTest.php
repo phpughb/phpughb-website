ddd540baa38b62f8bcbcfcbbf1ac5de8ff9cd1a3
Lucas Nothnagel <lucas.nothnagel@sensiolabs.de>
2020-02-25 07:27:04 +0100

    Add test for removal of userCreationToken

---
+++ tests/Unit/Domain/User/UserCreateTokenManagerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Domain\User;
+
+use App\Domain\User\TokenGenerator;
+use App\Domain\User\UserCreateTokenManager;
+use App\Domain\User\UserManager;
+use App\Entity\UserCreateToken;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Mailer\MailerInterface;
+use Twig\Environment;
+
+class UserCreateTokenManagerTest extends TestCase
+{
+    /**
+     * @test
+     * @covers \App\Domain\User\UserCreateTokenManager::activate
+     */
+    public function testUserCreateTokenIsRemovedAfterActivation(): void
+    {
+        $userCreateToken = new UserCreateToken('some-token', 'some@email.de', false);
+        $password = 'some-password';
+
+        $tokenGenerator = $this->prophesize(TokenGenerator::class);
+        $entityManager = $this->prophesize(EntityManagerInterface::class);
+        $mailer = $this->prophesize(MailerInterface::class);
+        $twig = $this->prophesize(Environment::class);
+        $userManager = $this->prophesize(UserManager::class);
+
+        $entityManager
+            ->remove($userCreateToken)
+            ->shouldBeCalled();
+        $entityManager
+            ->flush()
+            ->shouldBeCalled();
+
+        $userCreateTokenManager = new UserCreateTokenManager(
+            $tokenGenerator->reveal(),
+            $entityManager->reveal(),
+            $mailer->reveal(),
+            $twig->reveal(),
+            $userManager->reveal(),
+            'some-other@email.de'
+        );
+        $userCreateTokenManager->activate($userCreateToken, $password);
+    }
+}
